346039ea522609a8468a11e49493c3d5d9741b85
Username <jwaffensmith@gmail.com>
2021-09-01 13:01:44 -0700

    all quadrants working per assignment requirements

--- src/components/ArtSection.js
+++ src/components/ArtSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Loader from "./Loader";
+
 
 const contentStyles = {
 	position: "relative",
@@ -11,21 +11,15 @@ const contentStyles = {
 
 
 class ArtSection extends Component {
-	// 	state = {
-	// 	data: "",
-	// };
-
-	// componentDidMount() {
-	// 	this.setState({ data: this.props.apiData.images.preview_gif.url});
-	// }
-	
+		
 	render() {
+		const randomGif = this.props.apiData[Math.floor(Math.random()*this.props.apiData.length)] 
 		return (
 			<>
-			{this.props.apiData[0] ? 
+			{randomGif ? 
 			<section className="section">
 				<div style={contentStyles}>
-					<img alt="random gif" src={this.props.apiData[0].images.preview_gif.url} />
+					<img alt="random gif" src={randomGif.images.preview_gif.url} />
 				</div>
 			</section> : 
 			<>
@@ -36,45 +30,5 @@ class ArtSection extends Component {
 		)
 	}
 }
-
-// class ArtSection extends Component {
-// 	state = {
-// 		data: "",
-// // 	};
-// this.state.data && 
-// 	getRandomImage = (arr) => {
-// 		const rand = Math.random() * arr.length;
-// 		const index = Math.floor(rand);
-// 		const url = arr[index].images.preview_gif.url;
-// 		this.setState({ data: url });
-// 	};
-
-// 	handleOnClick = () => this.getRandomImage(this.props.apiData);
-
-// 	componentWillUnmount() {
-// 		this.handleOnClick();
-// 	}
-
-// 	generateContent = () => {
-// 		if (!this.state.data && this.props.apiData.length) {
-// 			this.getRandomImage(this.props.apiData);
-// 			return <Loader styles={contentStyles} />;
-// 		} else {
-// 			return (
-				// <div style={contentStyles}>
-				// 	<img alt="random gif" src={this.state.data} />
-				// </div>
-// 			);
-// 		}
-// 	};
-
-// 	render() {
-// 		return (
-// 			<section className="section" onClick={this.handleOnClick}>
-// 				{this.generateContent()} 
-// 			</section>
-// 		);
-// // 	}
-// }
 
 export default ArtSection;
